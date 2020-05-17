edd2a1754ebbb54b5aa03ac49b9576b0af683b72
Gabriele Musco <gabmus@disroot.org>
2020-05-17 18:43:16 +0200

    implemented notes deleting

--- notorious/files_listbox_row.py
+++ notorious/files_listbox_row.py
@@ -1,9 +1,13 @@
-from gi.repository import Gtk, Pango
+from gettext import gettext as _
+from gi.repository import Gtk, Gdk, Pango
+from os import remove
+from notorious.confManager import ConfManager
 
 
 class FileListboxRow(Gtk.ListBoxRow):
     def __init__(self, name, file_path, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.confman = ConfManager()
 
         self.name = name
         self.file_path = file_path
@@ -19,3 +23,26 @@ class FileListboxRow(Gtk.ListBoxRow):
             Pango.EllipsizeMode.END
         )
         self.add(self.name_label)
+        self.connect(
+            'key-press-event',
+            self.on_key_press_event
+        )
+
+    def on_key_press_event(self, widget, event):
+        if event.keyval == Gdk.KEY_Delete:
+            dialog = Gtk.MessageDialog(
+                self.get_toplevel(),
+                Gtk.DialogFlags.MODAL | Gtk.DialogFlags.DESTROY_WITH_PARENT,
+                Gtk.MessageType.QUESTION,
+                Gtk.ButtonsType.YES_NO,
+                _(
+                    'Delete note `{0}`?'
+                ).format(
+                    self.name
+                )
+            )
+            if dialog.run() == Gtk.ResponseType.YES:
+                remove(self.file_path)
+                self.confman.emit('notes_dir_changed', '')
+            dialog.close()
+
